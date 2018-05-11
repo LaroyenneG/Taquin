2c2faa3fe01c9d0c3c98c1ccaaa292c371a66bf6
laroyenneg <laroyenneg@gmail.com>
2018-05-11 15:43:26 +0200

    clean

---
+++ src/View/FBestScore.java
@@ -0,0 +1,100 @@
+package View;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FBestScore extends JFrame {
+    private JLabel nom1JLabel;
+    private JLabel nom2JLabel;
+    private JLabel nom3JLabel;
+    private JButton okJButton;
+    private String[] tbWinner;
+
+    public FBestScore(String[] tbWinner) {
+        this.tbWinner = tbWinner;
+        this.setTitle("Best scores");
+        this.initAttribut();
+        this.creerWidget();
+        this.afficheWinner();
+        this.setSize(450, 210);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        this.setAlwaysOnTop(true);
+        this.setVisible(true);
+    }
+
+    private void initAttribut() {
+        this.nom1JLabel = new JLabel("   1- Aucun joueur");
+        this.nom2JLabel = new JLabel("   2- Aucun joueur");
+        this.nom3JLabel = new JLabel("   3- Aucun joueur");
+        this.okJButton = new JButton("  OK  ");
+        this.okJButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                FBestScore.this.setVisible(false);
+            }
+        });
+    }
+
+    private void creerWidget() {
+        JPanel jPanel = new JPanel();
+        JPanel jPanel1 = new JPanel(new GridLayout(1, 2));
+        JPanel jPanel1_1 = new JPanel();
+        JPanel jPanel1_2 = new JPanel();
+        JPanel jPanel2 = new JPanel();
+        JPanel jPanel1_2_1 = new JPanel(new GridLayout(1, 1));
+        JPanel jPanel1_2_2 = new JPanel(new GridLayout(3, 1, 10, 10));
+        jPanel.setLayout(new BoxLayout(jPanel, 1));
+        jPanel1_1.setLayout(new BoxLayout(jPanel1_1, 1));
+        jPanel1_2.setLayout(new BoxLayout(jPanel1_2, 1));
+        jPanel2.setLayout(new BoxLayout(jPanel2, 0));
+        jPanel.setBackground(Color.WHITE);
+        jPanel1.setBackground(Color.WHITE);
+        jPanel2.setBackground(Color.WHITE);
+        jPanel1_1.setBackground(Color.WHITE);
+        jPanel1_2.setBackground(Color.WHITE);
+        jPanel1_2_1.setBackground(Color.WHITE);
+        jPanel1_2_2.setBackground(Color.WHITE);
+        JLabel titreJLabel = new JLabel("Les meilleurs scores sont :");
+        JLabel imageJLabel = new JLabel(new ImageIcon("resource/podium1.png"));
+        Font titre = new Font("Arial", 1, 14);
+        Font nom = new Font("Arial", 1, 12);
+        Font button = new Font("Arial", 1, 12);
+        titreJLabel.setFont(titre);
+        this.nom1JLabel.setFont(nom);
+        this.nom2JLabel.setFont(nom);
+        this.nom3JLabel.setFont(nom);
+        this.okJButton.setFont(button);
+        jPanel1_1.add(Box.createVerticalStrut(30));
+        jPanel1_1.add(imageJLabel);
+        jPanel1_2_1.add(titreJLabel);
+        jPanel1_2_2.add(this.nom1JLabel);
+        jPanel1_2_2.add(this.nom2JLabel);
+        jPanel1_2_2.add(this.nom3JLabel);
+        jPanel2.add(Box.createHorizontalStrut(280));
+        jPanel2.add(this.okJButton);
+        jPanel1_2.add(jPanel1_2_1);
+        jPanel1_2.add(Box.createVerticalStrut(15));
+        jPanel1_2.add(jPanel1_2_2);
+        jPanel1.add(jPanel1_1);
+        jPanel1.add(jPanel1_2);
+        jPanel.add(jPanel1);
+        jPanel.add(jPanel2);
+        this.setContentPane(jPanel);
+    }
+
+    private void afficheWinner() {
+        if (this.tbWinner.length == 1) {
+            this.nom1JLabel.setText("    1- " + this.tbWinner[0]);
+        } else if (this.tbWinner.length == 2) {
+            this.nom1JLabel.setText("    1- " + this.tbWinner[0]);
+            this.nom2JLabel.setText("    2- " + this.tbWinner[1]);
+        } else if (this.tbWinner.length >= 3) {
+            this.nom1JLabel.setText("    1- " + this.tbWinner[0]);
+            this.nom2JLabel.setText("    2- " + this.tbWinner[1]);
+            this.nom3JLabel.setText("    3- " + this.tbWinner[2]);
+        }
+
+    }
+}
---
+++ src/View/VueNewReccord.java
@@ -0,0 +1,124 @@
+package View;
+
+import Model.Model;
+
+import javax.swing.*;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class VueNewReccord extends JFrame {
+    private Model model;
+    private JTextField pseudoJTextField;
+    private JButton okJButton;
+    private JButton cancelJButton;
+    private JButton coupeJButton;
+    private JLabel titreJLabel;
+
+    public VueNewReccord(Model model) {
+        this.model = model;
+        this.initAttribut();
+        this.creerWidget();
+        this.setTitle("New Reccord");
+        this.setSize(510, 170);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        this.setAlwaysOnTop(true);
+        this.setVisible(true);
+    }
+
+    private String formatTemps() {
+        String chaine = "";
+        String brutChaine = Double.toString(this.model.getTime());
+
+        int i;
+        for (i = 0; i < brutChaine.length(); ++i) {
+            chaine = chaine + brutChaine.charAt(i);
+            if (brutChaine.charAt(i) == 46) {
+                chaine = chaine + brutChaine.charAt(i + 1);
+                i = brutChaine.length();
+            }
+        }
+
+        for (i = 0; i < 6 - Double.toString(this.model.getTime()).length(); ++i) {
+            chaine = chaine + " ";
+        }
+
+        return chaine;
+    }
+
+    private void initAttribut() {
+        this.pseudoJTextField = new JTextField();
+        this.okJButton = new JButton("   OK   ");
+        this.cancelJButton = new JButton("Cancel");
+        this.coupeJButton = new JButton(new ImageIcon("resource/coupe.png"));
+        this.titreJLabel = new JLabel("Felictations vous avez eu un meilleur score : " + this.formatTemps());
+    }
+
+    private void creerWidget() {
+        this.coupeJButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane message = new JOptionPane();
+                JOptionPane.showMessageDialog(message, "Tu es un champion !!", "Bravo", 1);
+            }
+        });
+        Font titreFont = new Font("Arial", 1, 13);
+        Font titreBisFont = new Font("Arial", 1, 12);
+        Font pseudoJTextFieldFont = new Font("Arial", 0, 13);
+        JPanel jPanel = new JPanel();
+        JPanel jPanel1 = new JPanel();
+        JPanel jPanel2 = new JPanel();
+        jPanel2.setLayout(new BoxLayout(jPanel2, 1));
+        JPanel jPanel3 = new JPanel();
+        jPanel3.setLayout(new BoxLayout(jPanel3, 0));
+        JLabel titreBisJLable = new JLabel("Veuillez saisir un pseudo :");
+        this.coupeJButton.setPreferredSize(new Dimension(80, 80));
+        titreBisJLable.setFont(titreBisFont);
+        this.titreJLabel.setFont(titreFont);
+        this.pseudoJTextField.setFont(pseudoJTextFieldFont);
+        jPanel1.add(this.coupeJButton);
+        jPanel2.add(this.titreJLabel);
+        jPanel2.add(Box.createVerticalStrut(10));
+        jPanel2.add(titreBisJLable);
+        jPanel2.add(Box.createVerticalStrut(5));
+        jPanel2.add(this.pseudoJTextField);
+        jPanel3.add(Box.createHorizontalStrut(260));
+        jPanel3.add(this.cancelJButton);
+        jPanel3.add(Box.createHorizontalStrut(50));
+        jPanel3.add(this.okJButton);
+        jPanel.add(jPanel1);
+        jPanel.add(jPanel2);
+        jPanel.add(jPanel3);
+        this.setContentPane(jPanel);
+    }
+
+    public void display() {
+        this.setVisible(true);
+    }
+
+    public void undisplay() {
+        this.setVisible(false);
+    }
+
+    public void setButtonControler(ActionListener listener) {
+        this.okJButton.addActionListener(listener);
+        this.cancelJButton.addActionListener(listener);
+    }
+
+    public void setTextControler(DocumentListener listener) {
+        this.pseudoJTextField.getDocument().addDocumentListener(listener);
+    }
+
+    public void resetText() {
+        this.pseudoJTextField.setText("");
+    }
+
+    public JTextField getPseudo() {
+        return this.pseudoJTextField;
+    }
+
+    public JButton getOkJButton() {
+        return this.okJButton;
+    }
+}
